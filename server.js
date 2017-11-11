5e92f8c3936d04700bdc614e763514dae8719828
Robert Morris <larcmo@gmail.com>
2017-11-10 17:18:44 -0700

    update to connection

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI  || "mongodb://localhost/nytReactArticles,
+  process.env.MONGODB_URI  || "mongodb://localhost/nytReactArticles",
   {
     useMongoClient: true
   }
